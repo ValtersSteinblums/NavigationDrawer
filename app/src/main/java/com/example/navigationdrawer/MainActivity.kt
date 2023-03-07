5121679383811876a5b4e469ce6db5dcc31e3e5f
ValtersSteinblums <109601588+ValtersSteinblums@users.noreply.github.com>
2023-03-07 17:42:53 +0200

    fragments

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.navigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.navigationdrawer.fragments.ChatFragment
+import com.example.navigationdrawer.fragments.MessageFragment
+import com.example.navigationdrawer.fragments.ProfileFragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawer: DrawerLayout
@@ -18,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         drawer = findViewById(R.id.drawer)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
         val toggle = ActionBarDrawerToggle(
             this@MainActivity,
             drawer,
@@ -27,8 +34,46 @@ class MainActivity : AppCompatActivity() {
         )
         drawer.addDrawerListener(toggle)
         toggle.syncState()
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, MessageFragment())
+            .commit()
+        navigationView.setCheckedItem(R.id.nav_message)
+
+        navigationView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_message -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, MessageFragment())
+                        .commit()
+                }
+                R.id.nav_chat -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, ChatFragment())
+                        .commit()
+                }
+                R.id.nav_profile -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, ProfileFragment())
+                        .commit()
+                }
+                R.id.nav_send -> {
+                    Toast.makeText(this@MainActivity, "Send pressed", Toast.LENGTH_SHORT).show()
+                }
+                R.id.nav_share -> {
+                    Toast.makeText(this@MainActivity, "Share pressed", Toast.LENGTH_SHORT).show()
+                }
+            }
+            drawer.closeDrawer(GravityCompat.START)
+            true
+        }
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START)
---
+++ app/src/main/java/com/example/navigationdrawer/fragments/ChatFragment.kt
@@ -0,0 +1,19 @@
+package com.example.navigationdrawer.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.navigationdrawer.R
+
+class ChatFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.chat_fragment, container, false)
+    }
+}
